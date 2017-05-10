5d1324d532eda9735be84743c373cc7a3bb7f76d
Curtis L. Olson <curtolson@flightgear.org>
2017-05-10 08:50:24 -0500

    Added an EKF annunciator.  This annunciator just flags unusually high bias values for now, but this metric can be sort of the canary in the mine.

--- annunciator.js
+++ annunciator.js
@@ -29,6 +29,7 @@ var annunciator = function() {
     
     function draw() {
         draw_sats();
+        draw_ekf();
         draw_power();
         draw_timer();
         draw_link_state();
@@ -46,6 +47,44 @@ var annunciator = function() {
                 sats_div.attr("class", "ok");
             }
             sats_div.html(sats + " sats");
+        }
+    };
+
+    function draw_ekf() {
+        var ekf_div = $("#ekf");
+        if ( ekf_div != null ) {
+            var gyro_warn = 0.5 * Math.PI / 180.0;
+            var gyro_error = 1.0 * Math.PI / 180.0;
+            var accel_warn = 0.5;
+            var accel_error = 1.0;
+            var p_bias = parseFloat(json.filters.filter[0].p_bias);
+            var q_bias = parseFloat(json.filters.filter[0].q_bias);
+            var r_bias = parseFloat(json.filters.filter[0].r_bias);
+            var ax_bias = parseFloat(json.filters.filter[0].ax_bias);
+            var ay_bias = parseFloat(json.filters.filter[0].ay_bias);
+            var az_bias = parseFloat(json.filters.filter[0].az_bias);
+            var imu_time = parseFloat(json.sensors.imu[0].timestamp);
+            var filter_time = parseFloat(json.filters.filter[0].timestamp);
+            var age = imu_time - filter_time;
+            if ( Math.abs(age) > 1.0 ||
+                 Math.abs(p_bias) >= gyro_error ||
+                 Math.abs(q_bias) >= gyro_error ||
+                 Math.abs(r_bias) >= gyro_error ||
+                 Math.abs(ax_bias) >= accel_error ||
+                 Math.abs(ay_bias) >= accel_error ||
+                 Math.abs(az_bias) >= accel_error ) {
+                ekf_div.attr("class", "error");
+            } else if ( Math.abs(age) > 0.5 ||
+                        Math.abs(p_bias) >= gyro_warn ||
+                        Math.abs(q_bias) >= gyro_warn ||
+                        Math.abs(r_bias) >= gyro_warn ||
+                        Math.abs(ax_bias) >= accel_warn ||
+                        Math.abs(ay_bias) >= accel_warn ||
+                        Math.abs(az_bias) >= accel_warn ) {
+                ekf_div.attr("class", "warn");
+            } else {
+                ekf_div.attr("class", "ok");
+            }
         }
     };
 
