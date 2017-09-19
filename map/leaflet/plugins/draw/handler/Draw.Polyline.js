c361b5460b9b646520b8dde740fe05b4b9638968
Curtis L. Olson <curtolson@flightgear.org>
2017-09-19 14:32:18 -0500

    Work around a leaflet bug (feature?) misdetecting my browswer as being a touch screen and turning off the live distance measurement in the tooltip.

--- map/leaflet/plugins/draw/handler/Draw.Polyline.js
+++ map/leaflet/plugins/draw/handler/Draw.Polyline.js
@@ -470,8 +470,9 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 	_getTooltipText: function () {
 		var showLength = this.options.showLength,
 			labelText, distanceStr;
-		if (L.Browser.touch) {
-			showLength = false; // if there's a better place to put this, feel free to move it
+	    if (L.Browser.touch) {
+                    // FIXME: I want length in tooltip, but my browser is detected as touch screen even though it isn't, so commenting out the next line...
+		    // showLength = false; // if there's a better place to put this, feel free to move it
 		}
 		if (this._markers.length === 0) {
 			labelText = {
