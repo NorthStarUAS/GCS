26c8d72ac5121f7544c82da51473140052280061
Curtis L. Olson <curtolson@flightgear.org>
2024-07-20 09:00:13 -0500

    rename current.py -> derived_states.py

--- nsLink/nslink.py
+++ nsLink/nslink.py
@@ -9,7 +9,7 @@ from PyQt6.QtWidgets import QLabel, QPushButton
 from PyQt6.QtCore import QTimer
 
 from commands import commands
-import current
+import nsLink.derived_states as derived_states
 from fmu_link import fmu_link
 import httpserver
 import joystick
@@ -39,7 +39,7 @@ fmu_link.begin(args.serial, args.baud, timeout=dt)
 def main_loop():
     sim_link.update()
     fmu_link.receive()
-    current.compute_derived_data()
+    derived_states.compute_derived_data()
     joystick.update()
     requests.gen_requests()
     commands.update()
