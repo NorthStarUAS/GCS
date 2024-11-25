8f73a373e5ac27805f90cc0182f91180fd0d95c9
Curtis L. Olson <curtolson@flightgear.org>
2024-11-24 20:54:15 -0600

    debug--

--- nsLink/derived_states.py
+++ nsLink/derived_states.py
@@ -51,9 +51,9 @@ class DerivedStates:
 
         # compute frame dt
         current_time = nav_node.getDouble('millis') / 1000.0
-        print("millis:", imu_node.getInt('millis'), nav_node.getDouble('millis'))
+        # print("millis:", imu_node.getInt('millis'), nav_node.getDouble('millis'))
         dt = current_time - self.last_time
-        print("cur: %.3f" % current_time, "dt: %.3f" % dt, "flt: %.3f" % self.flight_timer, "ap: %.3f" % self.ap_timer, "power: %.3f" % self.throttle_timer)
+        # print("cur: %.3f" % current_time, "dt: %.3f" % dt, "flt: %.3f" % self.flight_timer, "ap: %.3f" % self.ap_timer, "power: %.3f" % self.throttle_timer)
         self.last_time = current_time
 
         # local 'airborne' helper (not official)
@@ -61,7 +61,7 @@ class DerivedStates:
 
         # local autopilot timer
         ap_enabled = inceptors_node.getBool("master_switch")
-        print("is_airborne:", is_airborne, "ap:", ap_enabled)
+        # print("is_airborne:", is_airborne, "ap:", ap_enabled)
 
         # estimate odometer and timers
         if is_airborne:
