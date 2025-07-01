c42cf5e4e2fa97a3ae0cfc2b86bed81336a44bb0
Curtis L. Olson <curtolson@flightgear.org>
2025-07-01 17:42:06 -0500

    fiddle with map sizing.  Note there is no such thing as full height because web sites can be infinitely long and just scroll ... elements scale vertically but start with zero height.  If you make things full-height that's zero at the start! So there is view port height but this doesn't take into account the header and makes the content too tall.  But you can do vh - "n"rem and and rem is essentially the height of the font.  So it's a hack to guess how many rem high the header is, but it ends up working out ok here ... maybe not on the next person's display though.

--- nsLink/gui/main.py
+++ nsLink/gui/main.py
@@ -35,7 +35,23 @@ class Panel():
 
 class Map():
     def __init__(self):
-        ui.label("I'm the map")
+        # ui.label("I'm the map!")
+
+        # ui.context.client.content.classes('h-screen')
+        # ui.context.client.content.classes('h-[100vh]')
+        # ui.add_head_html('<style>.q-leaflet.flex-grow .q-field__control { height: 100% }</style>')
+
+        # self.map = ui.leaflet(center=(51.505, -0.09)).style("width:100%; height:800px;")
+        self.map = ui.leaflet(center=(51.505, -0.09)).classes("w-full h-[calc(100vh-10rem)]")
+        # self.map = ui.leaflet(center=(51.505, -0.09)).classes("w-full h-full")
+        # self.map = ui.leaflet(center=(51.505, -0.09)).classes("w-full flex-grow")
+
+        # https://github.com/zauberzeug/nicegui/discussions/4049
+        # self.map = ui.leaflet(center=(51.505, -0.09))
+        # ui.context.client.page_container.default_slot.children[0].props(
+        #     ''':style-fn="(offset, height) => ( { height: offset ? `calc(100vh - ${offset}px)` : '100vh' })"'''
+        # )
+        # ui.context.client.content.classes("h-full")
 
     @ui.refreshable
     async def update(self):
@@ -55,27 +71,27 @@ class MainDisplay():
         with ui.header(elevated=True).classes('items-center').style("font-size: 150%"):
             self.annunciator_bar = Annunciators()
             ui.space()
-            with ui.dropdown_button("Panel", auto_close=True) as tab_menu:
-                ui.item("Panel", on_click=lambda: ui.notify('You clicked item 1'))
-                ui.item("Map", on_click=lambda: ui.notify('You clicked item 1'))
-                ui.item("Data Bus", on_click=lambda: ui.notify('You clicked item 1'))
+            with ui.dropdown_button("Panel", auto_close=True) as self.tab_menu:
+                ui.item("Panel", on_click=lambda: self.my_select_tab("Panel"))
+                ui.item("Map", on_click=lambda: self.my_select_tab("Map"))
+                ui.item("Data Bus", on_click=lambda: self.my_select_tab("Data Bus"))
 
             ui.button(on_click=lambda: self.right_drawer.toggle(), icon='menu').props('flat color=white')
 
         with ui.right_drawer(fixed=False).style('background-color: #ebf1fa').props('bordered') as self.right_drawer:
-            self.event_log = ui.label('Event Log').style('white-space: pre-wrap')
+            self.event_log = ui.markdown('__Flight Notes:__').style('white-space: pre-wrap')
 
-        with ui.tabs().classes("w-full") as tabs:
+        with ui.tabs().classes("w-full") as self.tabs:
             panel = ui.tab("Panel", icon="speed")
             map = ui.tab("Map", icon="public")
             bus = ui.tab("Data Bus", icon="feed")
-        tabs.set_visibility(True)
+        self.tabs.set_visibility(False)
 
-        with ui.tab_panels(tabs, value=panel).classes('w-full'):
+        with ui.tab_panels(self.tabs, value=panel).classes('w-full'):
             with ui.tab_panel(panel):
                 self.panel = Panel()
                 self.panel.update()
-            with ui.tab_panel(map):
+            with ui.tab_panel(map).classes("h-full"):
                 self.map = Map()
                 self.map.update()
             with ui.tab_panel(bus):
@@ -86,13 +102,18 @@ class MainDisplay():
         ui.timer(0.1, self.map.update.refresh)
         ui.timer(0.1, self.bus.update.refresh)
 
+    def my_select_tab(self, name):
+        self.tabs.set_value(name)
+        self.tab_menu.set_text(name)
+        ui.notify("Selected: " + name)
+
     def update(self):
         self.annunciator_bar.update()
 
         result = event_mgr.get_next_event()
         if result is not None:
-            msg = "[%.1f] %s" % (result[0]/1000, result[1])
+            msg = "__[%.1f]__ %s" % (result[0]/1000, result[1])
             ui.notify(msg, position="top")
-            self.event_log.text += "\n" + msg
+            self.event_log.content += "\n" + msg
             print(event_mgr.pending_log)
 
