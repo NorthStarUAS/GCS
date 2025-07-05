6abd49b0ba6e829b027cf5734a02b4a24c3af722
Curtis L. Olson <curtolson@flightgear.org>
2025-07-04 21:50:13 -0500

    starting to work on the map aircraft icon, heading, location put recentering on a timer because I can't figure out how to compute if the aircraft is in the visible map bounds withing ui.leaflet (I had a pure leaflet/javascript way to do this though.)

--- nsLink/gui/map.py
+++ nsLink/gui/map.py
@@ -1,4 +1,7 @@
 from nicegui import ui
+import time
+
+from nodes import nav_node
 
 class Map():
     def __init__(self):
@@ -9,7 +12,9 @@ class Map():
         # ui.add_head_html('<style>.q-leaflet.flex-grow .q-field__control { height: 100% }</style>')
 
         # self.map = ui.leaflet(center=(51.505, -0.09)).style("width:100%; height:800px;")
-        self.map = ui.leaflet(center=(51.505, -0.09)).classes("w-full h-[calc(100vh-10rem)]")
+        self.map = ui.leaflet(center=(51.505, -0.09),
+                              additional_resources=['https://unpkg.com/leaflet-rotatedmarker@0.2.0/leaflet.rotatedMarker.js']
+                              ).classes("w-full h-[calc(100vh-10rem)]")
         # self.map = ui.leaflet(center=(51.505, -0.09)).classes("w-full h-full")
         # self.map = ui.leaflet(center=(51.505, -0.09)).classes("w-full flex-grow")
 
@@ -20,13 +25,26 @@ class Map():
         # )
         # ui.context.client.content.classes("h-full")
 
-        self.ownship = self.map.marker(latlng=self.map.center)
-        self.icon = 'L.icon({iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png"})'
-
-        self.post_init_done = False
+        self.map.marker(latlng=self.map.center, options={'rotationAngle': -30})
+        self.ownship = self.map.marker(latlng=self.map.center, options={'rotationAngle': -30, 'rotationOrigin': 'center'})
+        # self.icon = 'L.icon({iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png"})'
+        self.icon = 'L.icon({iconUrl: "/icons/fg_generic_craft.png", iconSize: [40, 40], iconAnchor: [20, 20]})'
+        self.setup_finished = False
+        self.centering_timer = 0
 
     @ui.refreshable
     async def update(self):
-        if True or not self.post_init_done:
-            self.post_init_done = True
-            # self.ownship.run_method(':setIcon', self.icon)
+        if not self.setup_finished:
+            await self.map.initialized()
+            print("map initialized")
+            self.ownship.run_method(':setIcon', self.icon)
+            self.setup_finished = True
+        self.ownship.move(nav_node.getDouble("latitude_deg"), nav_node.getDouble("longitude_deg"))
+        self.ownship.run_method('setRotationAngle', nav_node.getDouble("yaw_deg"))
+
+        current_time = time.time()
+        if current_time - self.centering_timer >= 10:
+            self.map.set_center((nav_node.getDouble("latitude_deg"), nav_node.getDouble("longitude_deg")))
+            self.centering_timer = current_time
+        # result = self.map.run_map_method('getBounds')
+        # print("bounds:", result)
