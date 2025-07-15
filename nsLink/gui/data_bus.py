de35676e687e7e1c5560e74374800827c3e0da85
Curtis L. Olson <clolson@cirrusaircraft.com>
2025-07-14 21:07:57 -0500

    migrate console functionality over to databus

--- nsLink/gui/data_bus.py
+++ nsLink/gui/data_bus.py
@@ -1,12 +1,34 @@
 from nicegui import ui
+from uuid import uuid4
 
 from PropertyTree import PropertyNode
 
+from commands import commands
+from event_mgr import command_mgr
+
 class DataBus():
     def __init__(self):
-        self.data = ui.label("").classes("font-mono").style("white-space: pre-wrap")
+        user = str(uuid4())
+        avatar = f'https://robohash.org/{user}?bgset=bg2'
+        with ui.row().classes('w-full items-center'):
+            with ui.avatar():
+                ui.image(avatar)
+            self.input = ui.input(label="Send command", placeholder='start typing') \
+                .props('rounded outlined').classes('flex-grow') \
+                .on('keydown.enter', self.send)
+        self.last_result = ui.markdown().style('w-full white-space: pre-wrap')
+        self.data = ui.label("").classes("w-full font-mono").style("white-space: pre-wrap")
+
+    def send(self):
+        commands.add(self.input.value)
+        self.input.value = ""
 
     @ui.refreshable
     async def update(self):
+        if len(command_mgr.results):
+            result = command_mgr.results[-1]
+            msg = "__[%.1f]__\n" % (result[0]/1000)
+            msg += "```%s```\n" % result[1]
+            self.last_result.content = msg
         json = PropertyNode("/").get_json_string()
         self.data.text = json
